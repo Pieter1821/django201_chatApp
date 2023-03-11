c5883ab5f9cd7c2b2cc4f761f5630c0b345bcd4b
Pieter Deane <deanepieter@gmail.com>
2023-03-11 22:00:42 +0200

    added static files

--- feed/views.py
+++ feed/views.py
@@ -1,6 +1,9 @@
-from django.views.generic import ListView
-
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post
+
+
 
 class HomePage(ListView):
     http_method_names = ["get"]
@@ -8,3 +11,28 @@ class HomePage(ListView):
     model = Post
     context_object_name = "posts"
     queryset = Post.objects.all().order_by('-id')[0:30]
+
+
+class PostDetailView(DetailView,LoginRequiredMixin):
+    http_method_names= ["get"]
+    model = Post
+    template_name = "feed/detail.html"
+    context_object_name = "post"
+
+
+class CreateNewPost(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = "feed/create.html"
+    fields = ['text']
+    success_url= "/"
+
+    def dispatch(self, request, *args, **kwargs):
+        self.request = request
+        return super().dispatch(request, *args, **kwargs)
+
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.author = self.request.user
+        obj.save()
+        return super().form_valid(form)
--- profiles/admin.py
+++ profiles/admin.py
@@ -6,3 +6,4 @@ class ProfileAdmin(admin.ModelAdmin):
 
 admin.site.register(Profile, ProfileAdmin)
 
+
